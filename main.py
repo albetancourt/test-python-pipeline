5eec433b1778547ac17dbb238b7aa764053ae5db
Alnardo Betancourt <betancourt.al@gmail.com>
2022-03-23 21:58:01 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/', methods=['POST'])
+def main():
+
+    return ('Hello!', 204)
+
+if __name__ == '__main__':
+    PORT = int(os.getenv('PORT')) if os.getenv('PORT') else 8080
+
+    # This is used when running locally. Gunicorn is used to run the
+    # application on Cloud Run. See entrypoint in Dockerfile.
+    app.run(host='127.0.0.1', port=PORT, debug=True)
